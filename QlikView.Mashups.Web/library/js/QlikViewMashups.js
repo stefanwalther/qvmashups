d844e33974d7bdcc88c8005697ee1e6e85c0767a
stefanwalther <stefan.walther@qlikblog.at>
2013-04-25 01:32:16 +0200

    Overall Changes (scripts, texts, ...)

--- QlikView.Mashups.Web/library/js/QlikViewMashups.js
+++ QlikView.Mashups.Web/library/js/QlikViewMashups.js
@@ -1,15 +1,61 @@
 ﻿
+// ------------------------------------------------------------------
+// Reference to demo.qlikview.com (ONLINE MODE)
+// ~~
+// - this will work for opendoc.htm & singleobject.htm Integration
+// - but this will not work for Div integration because of cross
+//   domain policy restrictions
+// ------------------------------------------------------------------
 var qvAjax_Path = 'http://eu.demo.qlikview.com/QvAJAXZfc/';
 var qvAjaxLib_Url = qvAjax_Path + 'htc/QvAjax.js';
 
-var docPath_SalesCompass = 'qvdocs/Sales%20Compass.qvw';
-var docPath_ITAssetManagement = 'qvdocs/IT%20Asset%20Management.qvw';
+var docPath_WhatsNew = 'qvdocs/Whats%20New%20in%20QlikView11.qvw';
+var docPath_SalesCompass = 'qvDocs/Sales%20Compass.qvw';
 
-// If you want to load your objects from your local machine (or any other machine)
-// uncomment the following line
+// ------------------------------------------------------------------
+// LOCAL SETUP
+// ~~
+// If you want to load your objects from your local machine (or any 
+// other machine) uncomment the following line and comment the 
+// variables above
 // In that scenario please make sure that the following Urls are working
-// var QVAJAX_PATH = 'http://localhost/QVAJAXZfc/';
+// ------------------------------------------------------------------
 
+var qvAjax_Path = 'http://localhost/QvAJAXZfc/';
+var qvAjaxLib_Url = qvAjax_Path + 'htc/QvAjax.js';
+
+var docPath_WhatsNew = 'Whats%20New%20in%20QlikView11.qvw';
+var docPath_SalesCompass = 'Sales%20Compass.qvw';
+
+
+
+
+
+
+
+// ------------------------------------------------------------------
+// Do not change code below this line ...
+// ------------------------------------------------------------------
+
+
+function IsOffline() {
+    return qvAjax_Path.indexOf('http://eu.demo.qlikview.com/') == -1;
+}
+
+function OfflineMsg() {
+
+    $OfflineMsg = $("<div id='dialogOffline'></div>");
+
+    $OfflineMsg.load("library/content/offlinedivmsg.html", function () {
+        $OfflineMsg.dialog({
+            width: 500,
+            height: 300,
+            title: 'Not Available in Online Mode',
+            resizable: false
+        });
+    });
+
+}
 
 function loadJs(url, cb) {
     var script = document.createElement('script');
@@ -25,7 +71,7 @@ function loadJs(url, cb) {
     script.onload = loadFunction;
     script.onreadystatechange = loadFunction;
     document.getElementsByTagName("head")[0].appendChild(script);
-};
+}
 
 $(function () {
 
